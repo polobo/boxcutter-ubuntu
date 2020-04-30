35f94f0cf478ee46d0a2f3f2d6baafd5961bd24a
David G. Johnston (DU) <davidj@dealeruplift.com>
2020-04-29 17:54:54 -0700

    Remove version conditional covering current versions.

--- script/update.sh
+++ script/update.sh
@@ -7,10 +7,8 @@ sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
 echo "==> Checking version of Ubuntu"
 . /etc/lsb-release
 
-if [[ $DISTRIB_RELEASE == 16.04 || $DISTRIB_RELEASE == 18.04 ]]; then
-    echo "==> Disabling periodic apt upgrades"
-    echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
-fi
+echo "==> Disabling periodic apt upgrades"
+echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
 echo "==> Updating list of repositories"
 # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
